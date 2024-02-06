1edeecaac6676c8f4f66e8f3fa2ebd20f5a9d1e6
BXJin <bsung472@gmail.com>
2024-02-06 15:00:30 +0900

    master first commit

---
+++ Source/Title2Lobby/FP_FirstPerson/FPSHUD.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSHUD.h"
+// 로직은 블루프린트에서 최대한 
---
+++ Source/Title2Lobby/Public/Game/GamePlayPC.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GamePlayPC.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TITLE2LOBBY_API AGamePlayPC : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
