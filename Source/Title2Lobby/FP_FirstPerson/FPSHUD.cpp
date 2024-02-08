71c365ab8aa44a970d22070f1f7904ded6edbf43
SangdoMarketJMLee <togoda1945@gmail.com>
2024-02-08 12:10:24 +0900

    Hud 코드 추가 UI 작동 확인

--- Source/Title2Lobby/FP_FirstPerson/FPSHUD.cpp
+++ Source/Title2Lobby/FP_FirstPerson/FPSHUD.cpp
@@ -2,4 +2,56 @@
 
 
 #include "FPSHUD.h"
+
+#include "FPSPlayerState.h"
+#include "Blueprint/UserWidget.h"
 // 로직은 블루프린트에서 최대한 
+void AFPSHUD::BeginPlay()
+{
+	Super::BeginPlay();
+	check(HudWidgetClass);
+
+	HudWidget = CreateWidget<UUserWidget>(GetWorld(), HudWidgetClass);
+	HudWidget->AddToViewport();
+
+	BindMyPlayerState();
+}
+
+void AFPSHUD::BindMyPlayerState()
+{
+	APlayerController* pc = GetWorld()->GetFirstPlayerController();
+	if (IsValid(pc))
+	{
+		AFPSPlayerState* ps = Cast<AFPSPlayerState>(pc->PlayerState);
+		if (IsValid(ps))
+		{
+			ps->m_Dele_UpdateHp.AddDynamic(this, &AFPSHUD::OnUpdateMyHp);
+			OnUpdateMyHp(ps->m_CurHp, 100);
+
+			
+			ps->m_Dele_UpdateAmmo.AddDynamic(this, &AFPSHUD::OnUpdateMyAmmo);
+			OnUpdateMyAmmo(ps->m_Ammo);
+			
+			ps->m_Dele_UpdateGold.AddDynamic(this, &AFPSHUD::OnUpdateMyGold);
+			OnUpdateMyGold(ps->m_Gold);
+			
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Blue, TEXT("AShootingHUD Bind Success!"));
+			return;
+		}
+	}
+
+	FTimerManager& timerManger = GetWorld()->GetTimerManager();
+	timerManger.SetTimer(th_BindMyPlayerState, this, &AFPSHUD::BindMyPlayerState, 0.01f, false);
+}
+
+void AFPSHUD::OnUpdateMyHp_Implementation(float CurHp, float MaxHp)
+{
+}
+
+void AFPSHUD::OnUpdateMyAmmo_Implementation(int Ammo)
+{
+}
+
+void AFPSHUD::OnUpdateMyGold_Implementation(int Gold)
+{
+}
